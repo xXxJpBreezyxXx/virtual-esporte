a6ec00879fee24960fff994c28e3c0ef3767386d
xXxJpBreezyxXx <joaopedrosilvajoao-silva2014@outlook.com>
2026-05-14 00:36:08 -0300

    tag google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,25 @@ export default function RootLayout({
       className={`${bebasNeue.variable} ${inter.variable} h-full`}
     >
       <body className="min-h-full flex flex-col font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T3CHKJLR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T3CHKJLR');`}
+        </Script>
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-MYMY0SSFEQ"
